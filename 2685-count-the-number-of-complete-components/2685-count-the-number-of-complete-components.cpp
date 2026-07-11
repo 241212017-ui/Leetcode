0de501882c324f4a103ef6b047fa97eb35a3873e
Razzak <241212017@nitdelhi.ac.in>
2026-07-11 14:09:27 +0530

    Time: 55 ms (45.96%), Space: 132.2 MB (53.99%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    void dfs(int i, unordered_map<int, vector<int>>& adj,vector<bool>& visited, int& v,
+             int& e) {
+
+        visited[i] = true;
+        v++;
+        e += adj[i].size();
+
+        for (int& neigh : adj[i]) {
+            if (!visited[neigh]) {
+                dfs(neigh, adj, visited, v, e);
+            }
+        }
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+
+        unordered_map<int, vector<int>> adj;
+
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(n, false);
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i])
+                continue;
+
+            int v = 0;
+            int e = 0;
+
+            dfs(i, adj, visited, v, e);
+
+            if (v * (v - 1) == e)
+                result++;
+        }
+
+        return result;
+    }
+};
