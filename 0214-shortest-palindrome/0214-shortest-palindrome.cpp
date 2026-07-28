1d7558b57a2f0d9c61903842c8011bf278d97736
Razzak <241212017@nitdelhi.ac.in>
2026-07-28 10:12:30 +0530

    Time: 16 ms (6.33%), Space: 12.4 MB (85.24%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+       string rev =s;
+       reverse(begin(rev), end(rev));
+       int n = s.length();
+       for(int i =0 ; i<n; i++){
+        if(!memcmp(s.c_str() , rev.c_str() +i, n-i)){
+        return rev.substr(0 , i)+s;
+        }
+       }
+       return rev +s;
+    }
+};
