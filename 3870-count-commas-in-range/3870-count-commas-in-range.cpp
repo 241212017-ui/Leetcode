a110e3fec85a90b3265829b62e49c9a92c3d02da
Razzak <241212017@nitdelhi.ac.in>
2026-09-08 08:04:11 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int count  =0;
-       if(n< 1000) return 0;
-       for(int num =1000; num<=n ; num++){
-        count ++;
-       }
-  return count;
+return n<1000?0: n-999;
     }
 };
